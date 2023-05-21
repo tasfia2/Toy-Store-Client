3308819491907d18ba1d4738b8a5af0c346b9ae7
tasfia2 <tasfiasaba22@gmail.com>
2023-05-21 18:25:22 +0600

    src

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes.jsx";
+import AuthProvider from "./providers/AuthProvider";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
